fd890cd68647419ae56724a32651ad1205d05c3c
Usuario <usuario@example.com>
2025-12-10 11:50:59 -0500

    agregar disparo automatico y cambios finales

---
+++ js/otra forma.js
@@ -0,0 +1,34 @@
+var total=0;
+var numProductos=0;
+
+function addFila() {
+    var tabla = document.getElementById("tblFactura").getElementsByTagName("tbody")[0];
+    var fila = tabla.insertRow(-1);
+    console.log(tabla);
+    var celda1 = fila.insertCell(0);
+    var celda2 = fila.insertCell(1);
+    var celda3 = fila.insertCell(2);  
+    var celda4 = fila.insertCell(3);
+    var celda5 = fila.insertCell(4);
+    var celda6 = fila.insertCell(5);
+
+    var botonEliminar = document.createElement("button");
+    botonEliminar.innerHTML = "-";
+    botonEliminar.onclick = function() {
+        var row = this.parentNode.parentNode;
+        row.remove();
+        var contador = document.getElementById("numProductos");
+        contador.innerHTML ="<b>"+ numProductos + "</b>";
+    }
+    celda1.appendChild(botonEliminar);
+
+    var caja1 = document.createElement("input");
+    caja1.id="codigo"+ids;
+    caja1.type = "text";
+    celda2.appendChild(caja1);
+
+    numProductos++;
+    ids++;
+    var contador = document.getElementById("numProductos");
+    contador.innerHTML = "<b>"+numProductos+"</b>";
+}
